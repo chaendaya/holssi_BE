7b079cfa4086299be1a142684524581b82515848
chaendaya <jini971@naver.com>
2024-07-26 04:18:55 +0900

    refactor : 어노테이션 추가

--- Project/holssi_BE/src/main/java/org/example/holssi_be/HolssiBeApplication.java
+++ Project/holssi_BE/src/main/java/org/example/holssi_be/HolssiBeApplication.java
@@ -2,12 +2,15 @@ package org.example.holssi_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "org.example.holssi_be.repository")
+@EntityScan(basePackages = "org.example.holssi_be.entity.domain")
 @EnableScheduling
 public class HolssiBeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HolssiBeApplication.class, args);
 	}
